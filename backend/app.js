db671c05b33d28661b5be32d1924bb874dd2c607
Therese <therese.holen22@gmail.com>
2023-03-20 12:32:14 +0100

    First commit, development environment for Express and Mongoose. Added DB with my name.

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+const PORT = 5000
+const mongoose = require('mongoose')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+async function init() {
+    try{
+        const options = {useNewUrlParser:true, useUnifiedTopology:true}
+        await mongoose.connect('mongodb://127.0.0.1:27017/ThereseHolen', options)
+        console.log("Connected to database")
+        app.listen(PORT, () => console.log(`Server is up and running on port: ${PORT}`))
+    } catch (error) {
+        console.error(error)
+    }
+}
+init()
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
