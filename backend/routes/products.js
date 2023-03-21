9f5e16c4e1923f32c210be768fdbfee43e56f28f
Therese <therese.holen22@gmail.com>
2023-03-21 12:38:13 +0100

    Added data to productroutes.

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,11 +1,52 @@
 var express = require('express');
 var router = express.Router();
-const UserModel = require('../models/user_models');
+//const UserModel = require('../models/user_models');
+const ProductModel = require('../models/product_models');
+const CategoriesModel = require('../models/categories_models');
 const CryptoJS = require("crypto-js");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-  });
+
+// HÄMTA ALLA PRODUKTER
+router.get('/', async(req, res) => {
+  try {
+    const products = await ProductModel.find().populate({path:'category', select:'name -_id'})
+    res.json(products)
+  } catch {
+    console.log(error)
+    res.status(400)
+  }
+});
+
+
+// HÄMTA SPECIFIK PRODUKT
+router.get('/:id', async(req, res) => {
+  try {
+    const _id = req.params.id
+    const product = await ProductModel.findOne({_id})
+    await product.populate({path:'category', select:'name -_id'})
+    res.json(product)
+  } catch (error){
+    console.log(error)
+    res.status(400)
+  }
+});
+
+
+// SKAPA PRODUKT // UTAN TOKEN SÅ SKALL ANROPET MISSLYCKAS = 401
+router.post('/add', async (req, res) => {
+  try {
+    if (req.body.token != "") {
+      const product = new ProductModel(req.body)
+      await product.save()
+      await product.populate({path:'category', select:'name -_id'})
+      res.status(201).json(product)
+    } else {
+      res.status(401).end()
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
+});
   
-  module.exports = router;
+module.exports = router;
