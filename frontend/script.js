ef737751e642bcd6df96862092da27fff11293c9
Therese <therese.holen22@gmail.com>
2023-03-23 21:41:32 +0100

    Added frontend JS. Implamented products and sortfunction.

--- frontend/script.js
+++ frontend/script.js
@@ -90,11 +90,146 @@ loginUserBtn.addEventListener("click", (event) => {
 /*-----------------------------------------------------------------------------
 ------------ Visa varorna utifrån kattegori ---------------------------------
 ----------------------------------------------------------------------------*/
+const productContainer = document.getElementById("product_container");
+
+//Hämta alla produkter.
+
+const allProducts = document.getElementById("all_products");
+allProducts.addEventListener("click", findAllProduct);
+
+function findAllProduct () {
+    fetch("http://localhost:3000/api/products", {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        //body:JSON.stringify(sendUserData)
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error("Network response was not OK")
+        }
+            return res.json()
+        })
+    .then(data => {
+        console.log(data);
+        productContainer.innerHTML="";
+        printProducts(data);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        //const loginMassage = document.getElementById("login_massage");
+
+        //const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
+        loginMessage.style.color = "red";
+        loginMessage.appendChild(errorMassege);
+
+    });
+}
 
 
-//Lägga in eventlistner på mina buttons. Dessa ska trigga en get till anropet av en specifik kategori. 
+//Hämta alla pruduker med kategori Kökstextil 
+
+const kitchenProducts = document.getElementById("sort_kitchen");
+kitchenProducts.addEventListener("click", findAllProductByKitchen);
+
+function findAllProductByKitchen () {
+    fetch("http://localhost:3000/api/products/category/64199abf6b236bee1295fcc2", {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        //body:JSON.stringify(sendUserData)
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error("Network response was not OK")
+        }
+            return res.json()
+        })
+    .then(data => {
+        console.log(data);
+        productContainer.innerHTML="";
+        printProducts(data)
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        //const loginMassage = document.getElementById("login_massage");
+
+        //const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
+        loginMessage.style.color = "red";
+        loginMessage.appendChild(errorMassege);
+
+    });
+}
 
 
+//Hämta alla produkter med kategori Badtextil 
+
+const bathroomProducts = document.getElementById("sort_bathroom");
+bathroomProducts.addEventListener("click", findAllProductBathroom);
+
+function findAllProductBathroom () {
+    fetch("http://localhost:3000/api/products/category/64199acf6b236bee1295fcc4", {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        //body:JSON.stringify(sendUserData)
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error("Network response was not OK")
+        }
+            return res.json()
+        })
+    .then(data => {
+        console.log(data);
+        productContainer.innerHTML="";
+        printProducts(data)
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        //const loginMassage = document.getElementById("login_massage");
+       // const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
+        loginMessage.style.color = "red";
+        loginMessage.appendChild(errorMassege);
+
+    });
+}
+
+
+//Hämta alla produkter med kategori Sovrum 
+const bedroomProducts = document.getElementById("sort_bedroom");
+bedroomProducts.addEventListener("click", findAllProductBedroom);
+
+function findAllProductBedroom () {
+    fetch("http://localhost:3000/api/products/category/64199adc6b236bee1295fcc6", {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        //body:JSON.stringify(sendUserData)
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error("Network response was not OK")
+        }
+            return res.json()
+        })
+    .then(data => {
+        console.log(data);
+        productContainer.innerHTML="";
+        printProducts(data)
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+        //const loginMassage = document.getElementById("login_massage");
+        //const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
+        loginMessage.style.color = "red";
+        loginMessage.appendChild(errorMassege);
+    });
+}
 
 
 //Namn, pris och bild ska loopas ut. 
@@ -114,23 +249,24 @@ function printProducts(products) {
         const productImg = document.createElement("img");
         productImg.class = "product_img";
         productImg.id = "product_img";
+        productImg.setAttribute("src", "src/img/Exempelbild.jpg");   
+        productImg.setAttribute("width", "304");
+        productImg.setAttribute("height", "228");
+        productImg.setAttribute("alt", "Varan");
+
         productArticle.appendChild(productImg);
 
-        const productName = document.createElement("h4");
-        productName.class = "product_name";
-        productName.id = "product_name";
-        productArticle.appendChild(productName);
+        const productNameH4 = document.createElement("h4");
+        productNameH4.class = "product_name";
+        productNameH4.id = "product_name";
+        productArticle.appendChild(productNameH4);
+        productNameH4.innerHTML = product.name;
 
-        const productPrice = document.createElement("span");
+        const productPrice = document.createElement("p");
         productPrice.class = "product_price";
         productPrice.id = "product_price";
         productArticle.appendChild(productPrice);
-
-
-        const byProductBtn = document.createElement("button");
-        byProductBtn.id = product.id
-        byProductBtn.innerHTML = "Köp"
-        productArticle.appendChild(byProductBtn);
+        productPrice.innerHTML = product.price + "kr";
 
         const numberOfGoods = document.createElement("INPUT"); 
         numberOfGoods.setAttribute("type", "number");
@@ -138,12 +274,10 @@ function printProducts(products) {
         numberOfGoods.id = "number_of_goods";
         productArticle.appendChild(numberOfGoods);
 
-        const inputNewTitle = document.createElement("INPUT");
-        inputNewTitle.
-        inputNewTitle.
-        inputNewTitle.
-        inputNewTitle.
-        newBookForm.appendChild(inputNewTitle);
+        const byProductBtn = document.createElement("button");
+        byProductBtn.id = product.id
+        byProductBtn.innerHTML = "Köp"
+        productArticle.appendChild(byProductBtn);
 
         byProductBtn.addEventListener("click", (event) => {
             //root.innerHTML="";
