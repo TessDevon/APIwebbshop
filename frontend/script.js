b90fb2a4805fbf43baac19cc471843bca32940f9
Therese <therese.holen22@gmail.com>
2023-03-25 14:45:09 +0100

    feat. Fixes category from DB. Added basket. Added send order and update DB.

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,8 @@
 
+/*------------------------------------------------------------------------------------------------
+--------------------------- Skapa ny användare ---------------------------------------------------
+------------------------------------------------------------------------------------------------*/ 
 
-/*----------------------------------------------------------------------------
------------- Skapa ny användare ----------------------------------------------
-----------------------------------------------------------------------------*/ 
 const newUserBtn = document.getElementById("new_user_btn");
 const newName = document.getElementById("new_name");
 const newEmail = document.getElementById("new_email");
@@ -24,31 +24,30 @@ newUserBtn.addEventListener("click", (event) => {
     })
     .then(res => res.json())
     .then(data => {
-//        printUsers(data);
-        console.log(data);
     });
     newName.value = ""
     newEmail.value = ""
     newPassword.value = ""
 })
 
-/*----------------------------------------------------------------------------
------------- Logga in användare ----------------------------------------------
-----------------------------------------------------------------------------*/ 
+
+
+/*-------------------------------------------------------------------------------------------------
+---------------------------- Logga in användare ---------------------------------------------------
+-------------------------------------------------------------------------------------------------*/ 
+
 const loginUserBtn = document.getElementById("login_user_btn");
 const loginUserEmail = document.getElementById("login_email");
 const loginUserPassword = document.getElementById("login_password");
 const loginMessage = document.getElementById("login_message");
 
+let userIdLocalStorage = JSON.parse(localStorage.getItem("userIdLocalStorage")) || {id:""};
+
 let userId;
 loginUserBtn.addEventListener("click", (event) => {
     event.preventDefault();
     let sendUserData = {email: loginUserEmail.value, password: loginUserPassword.value};
-    
-    // logIn.reset();
     loginMessage.innerHTML = ""
-
-    // console.log(user);
 
     fetch("http://localhost:3000/api/users/login", {
         method: "POST",
@@ -65,7 +64,8 @@ loginUserBtn.addEventListener("click", (event) => {
         })
     .then(data => {
         console.log(data);
-        userId = data;
+        userId = data.id;
+        localStorage.setItem("userIdLocalStorage", JSON.stringify({id:userId}))
         console.log(userId)
         
         //const serverMassage = document.getElementById("serverMassage");
@@ -80,20 +80,20 @@ loginUserBtn.addEventListener("click", (event) => {
         const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
         loginMessage.style.color = "red";
         loginMessage.appendChild(errorMassege);
-
     });
     loginUserEmail.value = ""
     loginUserPassword.value = ""
-    
 });
 
-/*-----------------------------------------------------------------------------
------------- Visa varorna utifrån kattegori ---------------------------------
-----------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------------------------------
+------------------------- Visa varorna utifrån kattegori --------------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
 const productContainer = document.getElementById("product_container");
 
 //Hämta alla produkter.
-
 const allProducts = document.getElementById("all_products");
 allProducts.addEventListener("click", findAllProduct);
 
@@ -126,20 +126,60 @@ function findAllProduct () {
 
     });
 }
+findAllProduct();
 
-
-//Hämta alla pruduker med kategori Kökstextil 
-
-const kitchenProducts = document.getElementById("sort_kitchen");
-kitchenProducts.addEventListener("click", findAllProductByKitchen);
-
-function findAllProductByKitchen () {
-    fetch("http://localhost:3000/api/products/category/64199abf6b236bee1295fcc2", {
+function laodCategories () {
+    fetch("http://localhost:3000/api/categories", {
         method:"GET",
         headers: {
             "Content-Type": "application/json",
         },
-        //body:JSON.stringify(sendUserData)
+
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error("Network response was not OK")
+        }
+            return res.json()
+        })
+    .then(data => {
+        const sortDiv = document.getElementById("sort_div");
+        let categorys = data
+        categorys.map(category => {
+
+            const categoryRadio = document.createElement("INPUT"); 
+            categoryRadio.setAttribute("type", "radio");
+            categoryRadio.setAttribute("name","sort");
+            categoryRadio.id = category._id;
+            console.log(category)
+            sortDiv.appendChild(categoryRadio);
+
+            const categoryText = document.createElement("span");
+            categoryText.class = "category_text";
+            categoryText.id = "category_text";
+            sortDiv.appendChild(categoryText);
+            categoryText.innerHTML = category.name;
+
+            categoryRadio.addEventListener("click", findAndWhriteCategory)
+        })
+    })
+};
+laodCategories();
+
+
+
+//Hämta produkter från en kategori och skriv ut
+
+function findAndWhriteCategory (event) {
+
+    const selectedCategory = event.target.id
+    console.log(selectedCategory);
+
+    fetch("http://localhost:3000/api/products/category/" + selectedCategory, {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
     })
     .then(res => {
         if(!res.ok) {
@@ -154,96 +194,25 @@ function findAllProductByKitchen () {
     })
     .catch((error) => {
         console.error("Error:", error);
-        //const loginMassage = document.getElementById("login_massage");
-
-        //const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
         loginMessage.style.color = "red";
         loginMessage.appendChild(errorMassege);
 
     });
 }
 
-
-//Hämta alla produkter med kategori Badtextil 
-
-const bathroomProducts = document.getElementById("sort_bathroom");
-bathroomProducts.addEventListener("click", findAllProductBathroom);
-
-function findAllProductBathroom () {
-    fetch("http://localhost:3000/api/products/category/64199acf6b236bee1295fcc4", {
-        method:"GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        //body:JSON.stringify(sendUserData)
-    })
-    .then(res => {
-        if(!res.ok) {
-            throw new Error("Network response was not OK")
-        }
-            return res.json()
-        })
-    .then(data => {
-        console.log(data);
-        productContainer.innerHTML="";
-        printProducts(data)
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-        //const loginMassage = document.getElementById("login_massage");
-       // const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
-        loginMessage.style.color = "red";
-        loginMessage.appendChild(errorMassege);
-
-    });
-}
-
-
-//Hämta alla produkter med kategori Sovrum 
-const bedroomProducts = document.getElementById("sort_bedroom");
-bedroomProducts.addEventListener("click", findAllProductBedroom);
-
-function findAllProductBedroom () {
-    fetch("http://localhost:3000/api/products/category/64199adc6b236bee1295fcc6", {
-        method:"GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        //body:JSON.stringify(sendUserData)
-    })
-    .then(res => {
-        if(!res.ok) {
-            throw new Error("Network response was not OK")
-        }
-            return res.json()
-        })
-    .then(data => {
-        console.log(data);
-        productContainer.innerHTML="";
-        printProducts(data)
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-        //const loginMassage = document.getElementById("login_massage");
-        //const errorMassege = document.createTextNode("Error! Fel användarnamn eller lösenord!");
-        loginMessage.style.color = "red";
-        loginMessage.appendChild(errorMassege);
-    });
-}
 
 
 //Namn, pris och bild ska loopas ut. 
+
 function printProducts(products) {
 
     const productContainer = document.getElementById("product_container");
 
-
-
     products.map(product => {
         
         const productArticle = document.createElement("article");
-        productArticle.class = "product";
-        productArticle.id = "product";
+        productArticle.class = "product_article ";
+        productArticle.id = "product_article";
         productContainer.appendChild(productArticle);  
 
         const productImg = document.createElement("img");
@@ -268,40 +237,218 @@ function printProducts(products) {
         productArticle.appendChild(productPrice);
         productPrice.innerHTML = product.price + "kr";
 
-        const numberOfGoods = document.createElement("INPUT"); 
-        numberOfGoods.setAttribute("type", "number");
-        numberOfGoods.className = "number_of_goods";
-        numberOfGoods.id = "number_of_goods";
-        productArticle.appendChild(numberOfGoods);
+        const buyProductBtn = document.createElement("button");
+        buyProductBtn.id = product._id
+        buyProductBtn.innerHTML = "Köp"
+        productArticle.appendChild(buyProductBtn);
 
-        const byProductBtn = document.createElement("button");
-        byProductBtn.id = product.id
-        byProductBtn.innerHTML = "Köp"
-        productArticle.appendChild(byProductBtn);
+        buyProductBtn.addEventListener("click", addProductToBasket)
 
-        byProductBtn.addEventListener("click", (event) => {
-            //root.innerHTML="";
-            //showBook(event);
-        }); 
     });
 };
 
 
-/*----------------------------------------------------------------------------
------------- Kundvagnen ------------------------------------------------------
-----------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------
+------------------------------------- Kundvagnen ----------------------------------------------------------
+---------------------------------------------------------------------------------------------------------*/
+
+/*---------------- Lägga till varan i kundvagnen som sparas på localhost. --------------------------------*/
+
+function addProductToBasket (event) {
+    let Basket = JSON.parse(localStorage.getItem("Basket")) || [];
+
+    const addProductId = event.target.id
+
+    const itemInBasket = Basket.find(basketProduct => basketProduct.productId === addProductId)
+    if (itemInBasket) {
+        itemInBasket.quantity ++;
+    } else {
+        const updatedBasket = [...Basket, {productId:addProductId, quantity: 1}]
+        Basket = updatedBasket;   
+    }
+    localStorage.setItem("Basket", JSON.stringify(Basket))
+    whriteBasket();
+}
+
+
+/*------------------ Kundvagnen skrivs ut genom att hämta lista från lokahost ------------------------------------------*/
+
+function whriteBasket () {
+    let updatedBasket = JSON.parse(localStorage.getItem("Basket")) || []
+
+    const shopBasketContainer = document.getElementById("shop_basket_container");
+
+    shopBasketContainer.innerHTML= "";
+
+    updatedBasket.forEach(basketProduct => 
+        {
+        fetch("http://localhost:3000/api/products/" + basketProduct.productId, {
+            method:"GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        .then(res => {
+            if(!res.ok) {
+                throw new Error("Network response was not OK")
+            }
+                return res.json()
+            })
+        .then(product => {
+            const basketArticle = document.createElement("article");
+            basketArticle.class = "basket_product_article";
+            basketArticle.id = "basket_product_article";
+            shopBasketContainer.appendChild(basketArticle); 
+
+            const basketProductImg = document.createElement("img");
+            basketProductImg.class = "basket_product_img";
+            basketProductImg.id = "basket_product_img";
+            basketProductImg.setAttribute("src", "src/img/Exempelbild.jpg");   
+            basketProductImg.setAttribute("width", "304");
+            basketProductImg.setAttribute("height", "228");
+            basketProductImg.setAttribute("alt", "Varan");
+            basketArticle.appendChild(basketProductImg);
+
+            const productNameBasket = document.createElement("h4");
+            productNameBasket.class = "basket_product_name";
+            productNameBasket.id = "basket_product_name";
+            basketArticle.appendChild(productNameBasket);
+            productNameBasket.innerHTML = product.name;
+
+            const productPriceInBasket = document.createElement("p");
+            productPriceInBasket.class = "product_price_basket";
+            productPriceInBasket.id = "product_price_basket";
+            basketArticle.appendChild(productPriceInBasket);
+            productPriceInBasket.innerHTML = product.price + "kr";
+
+            const productQuantity = document.createElement("p");
+            productQuantity.class = "product_price_basket";
+            productQuantity.id = "product_price_basket";
+            basketArticle.appendChild(productQuantity);
+            productQuantity.innerHTML = basketProduct.quantity + "st";
+        })
+        .catch((error) => {
+        console.error("Error:", error);
+        });
+    });
+ };
+
+whriteBasket();
 
 
 
-/*---------------------------------------------------------------------------
------------- Skicka order och uppdatera saldor i DB -------------------------
-----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------
+------------------------------------ Skicka order och uppdatera saldor i DB --------------------------------------
+----------------------------------------------------------------------------------------------------------------*/
+
+const orderBtn = document.getElementById("order_btn");
+
+orderBtn.addEventListener("click", sendOrder);
+
+function sendOrder() {
+    let userIdLocalStorage = JSON.parse(localStorage.getItem("userIdLocalStorage")) || {id:""};
+    let orderBasket = JSON.parse(localStorage.getItem("Basket")) || []
+
+    fetch("http://localhost:3000/api/orders/add", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({user: userIdLocalStorage.id, products: orderBasket})
+        })
+        .then(res => {
+            if(!res.ok) {
+                throw new Error("Network response was not OK")
+            }
+                return res.json()
+            })
+        .then(res => {
+            localStorage.setItem("Basket", JSON.stringify([]));
+            whriteBasket();
+        })
+
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+};
 
 
 
-/*---------------------------------------------------------------------------
------------- Visa användarens ordrarna på ny sida ---------------------------------------
-----------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------
+----------------------------------- Visa användarens ordrarna på ny sida ---------------------------------------
+--------------------------------------------------------------------------------------------------------------*/
+
+function fetchOrders (event) {
+
+    let userIdLocalStorage = JSON.parse(localStorage.getItem("userIdLocalStorage")) || {id:""};
+
+    fetch("http://localhost:3000/api/orders/user", {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({user: userIdLocalStorage.id})
+    })
+    .then(res => {
+        if(!res.ok) {
+            throw new Error("Network response was not OK")
+        }
+            return res.json()
+        })
+    .then(orders => {
+        whriteOrders(orders);
+    })
+
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+};
 
 
-//Spara inloggning och kundvagn i Localhost
+    function whriteOrders(orders) {
+    const userOrderContainer = document.getElementById("user-order_container");
+    console.log(data);
+
+    orders.map(order => {
+        const orderArticle = document.createElement("article");
+        orderArticle.class = "order_article ";
+        orderArticle.id = "order_article";
+        userOrderContainer.appendChild(orderArticle);  
+
+        const orderNumber = document.createElement("h3");
+        orderNumber.class = "order_number";
+        orderNumber.id = "order_number";
+        orderArticle.appendChild(productNameBasket);
+    //    productNameBasket.innerHTML = product.name;
+
+        const orderProductImg = document.createElement("img");
+        orderProductImg.class = "order_product_img";
+        orderProductImg.id = "order_product-img";
+        orderProductImg.setAttribute("src", "src/img/Exempelbild.jpg");   
+        orderProductImg.setAttribute("width", "304");
+        orderProductImg.setAttribute("height", "228");
+        orderProductImg.setAttribute("alt", "Varan");
+        orderArticle.appendChild(orderProductImg);
+
+        const productNameOrder = document.createElement("h4");
+        productNameOrder.class = "product_name_order";
+        productNameOrder.id = "product_name_order";
+        orderArticle.appendChild(productNameOrder);
+    //    productNameBasket.innerHTML = product.name;
+
+        const productPriceInBasket = document.createElement("p");
+        productPriceInBasket.class = "product_price_basket";
+        productPriceInBasket.id = "product_price_basket";
+        orderArticle.appendChild(productPriceInBasket);
+        productPriceInBasket.innerHTML = product.price + "kr";
+
+        const numberOfGoodsBasket = document.createElement("p"); 
+        numberOfGoodsBasket.className = "number_of_goods_basket";
+        numberOfGoodsBasket.id = "number_of_goods_basket";
+        orderArticle.appendChild(numberOfGoodsBasket);
+        numberOfGoodsBasket.innerHTML = numberOfGoods + "st";
+    });   
+};
